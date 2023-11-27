6099821dd2fc27911b8f8efe734550ca3fe5a893
Larky <larky0071@gmail.com>
2023-11-27 08:20:59 +0300

    upd 2.0

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Gallery from './Gallery';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <Gallery />
   </React.StrictMode>,
 );
